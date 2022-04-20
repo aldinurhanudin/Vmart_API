3db42154d4432dd52e8516fb6736e5c9d647ef03
sahrul1610 <sahrulfyou@gmail.com>
2022-04-20 12:33:45 +0700

    API produk & pesanan

--- web_vmart/app/Http/Controllers/API/OrderItemController.php
+++ web_vmart/app/Http/Controllers/API/OrderItemController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class OrderItemController extends Controller
 {
-    //
+    public function index(){
+
+        $data = order_item::all();
+
+
+        return response()->json(['messege' => 'request success', 'data'=>$data],200);
+    }
+
+    public function insert(Request $request){
+
+
+
+
+        $data = order_item::create($request->all());
+        return response()->json(['messege' => 'request success', 'data' => $data],200);
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $order_item = order_item::find($id);
+        $order_item->update($request->all());
+
+
+        return response()->json(['messege' => 'request success', 'data' => $order_item],200);
+    }
+    public function destroy($id)
+    {
+        $order_item = order_item::find($id);
+        $order_item->delete();
+
+
+        return response()->json(['messege' => 'request success', 'data' => null],200);
+    }
+
 }
