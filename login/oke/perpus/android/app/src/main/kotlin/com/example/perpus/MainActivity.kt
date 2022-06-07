d0113e3eec1a97b318aa0f0b183da86a917ac244
sahrul1610 <sahrulfyou@gmail.com>
2022-06-07 15:25:42 +0700

    pemindahan dokumen proyek3

--- login/oke/perpus/android/app/src/main/kotlin/com/example/perpus/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.perpus
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- login/oke/perpus/lib/pages/pages.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-// import 'package:login_ui_master/shared/shared.dart';
-import 'package:perpus/shared/shared.dart';
-
-import '../main.dart';
-
-part 'welcome_pages.dart';
